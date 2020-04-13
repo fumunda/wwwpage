ffa93d9cdae4c98d7d0aaef0b4cced940d459df5
Eugen Ciur <eugen@django-lessons.com>
2020-04-13 15:48:40 +0200

    german translation for papermerge.de

--- land/migrations/0001_initial.py
+++ land/migrations/0001_initial.py
@@ -19,7 +19,7 @@ class Migration(migrations.Migration):
             ],
         ),
         migrations.CreateModel(
-            name='Subscribtion',
+            name='Subscription',
             fields=[
                 ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
                 ('email', models.EmailField(max_length=254)),
